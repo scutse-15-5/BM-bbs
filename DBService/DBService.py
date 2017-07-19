ffdc1d03ce3c7003cbf53329310827afb1a25047
IMCvb <13416349304@163.com>
2017-07-19 22:42:53 +0800

    add select funtion

--- DBService/DBService.py
+++ DBService/DBService.py
@@ -1,7 +1,6 @@
-# 为了实现中文插入，运行这个脚本之前把Python36\Lib\site-packages\pymysql\connection.py的第105行的DEFAULT_CHARSET = 'latin1'改成
-import pymysql
-import time
+# 为了实现中文插入，运行这个脚本之前把Python36\Lib\site-packages\pymysql\connection.py的第105行的DEFAULT_CHARSET = 'latin1'改成'utf8'
 
+import pymysql
 
 class DBService:
     # 这些是数据库连接的信息，后期改成配置文件导入的方式读取
@@ -11,8 +10,16 @@ class DBService:
     __dbname = "bmbbs"
     __db = pymysql.connect(__dbsource, __user, __password, __dbname)
     __cursor = __db.cursor()
-
+    #单例
+    __dbService = None
+    def __init__(self):
+        pass
+    def __new__(cls, *args, **kw):
+        if not cls.__dbService:
+            cls.__instance = super(DBService, cls).__new__(cls, *args, **kw)
+        return cls.__instance
     # 支持输入数字和字符串
+
     def inserNewAccount(self, name, password):
         ifInUse = 1
         self.__cursor.execute("SELECT COUNT(*) FROM ACCOUNT WHERE NAME LIKE '%s'" % name)
@@ -43,9 +50,10 @@ class DBService:
         newNoticeTuple = ["NOTICE", partname, content]
         return self.__insertTuple(newNoticeTuple)
 
-    def __insertTuple(self, tuplelist: list):
-        sql = "INSERT INTO " + str(tuplelist[0]) + " VALUE("
-        for element in tuplelist[1:]:
+    #插入元组
+    def __insertTuple(self, tupleList):
+        sql = "INSERT INTO " + str(tupleList[0]) + " VALUE("
+        for element in tupleList[1:]:
             sql += "'" + str(element) + "',"
         sql = sql[0:-1]
         sql += ")"
@@ -56,20 +64,21 @@ class DBService:
         except:
             self.__db.rollback()
             return 0
-	#修改元组
-    def updateTuple(self, table: str, editAttribute: list, selectAttribute: list):
+
+    #修改元组
+    def updateTuple(self, table, editAttri, selectFactor):
         sql = "UPDATE %s SET " % table
-        length  = len(editAttribute)
+        length  = len(editAttri)
         index = 0
         while index < length:
-            sql += str(editAttribute[index]) + " = '" + str(editAttribute[index + 1]) + "', "
+            sql += str(editAttri[index]) + " = '" + str(editAttri[index + 1]) + "', "
             index += 2
         sql = sql[:-2]
-        length = len(selectAttribute)
+        length = len(selectFactor)
         index = 0
         sql += " WHERE "
         while index < length:
-            sql += str(selectAttribute[index]) + " = '" + str(selectAttribute[index + 1]) + "' AND "
+            sql += str(selectFactor[index]) + " = '" + str(selectFactor[index + 1]) + "' AND "
             index += 2
         sql = sql[:-4]
 
@@ -80,15 +89,16 @@ class DBService:
         except:
             self.__db.rollback()
             return 0
-	#删除元组
-    def deleteTuple(self, table: str, selectAttribute: list):
+
+    #删除元组
+    def deleteTuple(self, table, selectFactor):
         sql = "DELETE FROM %s WHERE " % table
 
-        length =len(selectAttribute)
+        length =len(selectFactor)
         index = 0
 
         while index < length:
-            sql += str(selectAttribute[index]) + " = '" + str(selectAttribute[index + 1]) + "' AND "
+            sql += str(selectFactor[index]) + " = '" + str(selectFactor[index + 1]) + "' AND "
             index += 2
         sql = sql[:-4]
 
@@ -100,6 +110,31 @@ class DBService:
             self.__db.rollback()
             return 0
 
+    def selectTuple(self, table, selectAttri, selectFactor):
+        sql = "SELECT "
+        for element in selectAttri:
+            sql += str(element) + ", "
+        sql = sql[:-2] + " WHERE "
+
+        index = 0
+        while index < len(selectFactor):
+            sql += str(selectFactor[index]) + " = '" + str(selectFactor[index + 1]) + "' AND "
+            index += 2
+        sql = sql[:-4]
+        self.__cursor.execute(sql)
+        return self.__cursor.fetchall()
 
 
-#这个版本是最初版本0.1，经过一些测试没有大bug
+
+
+
+
+
+'''
+这个版本是最初版本0.1，经过一些测试没有大bug
+
+以下是调用这些函数的实例：
+instance = DBService()
+instance.insertNewAccount('name','password') 所有的插入函数都是一样的调用方法
+instance.updateTuple('ACCOUNT', ['要更改的属性', ‘更新值'，.......], ['选择的属性', '选择值',......]) 选择也是一样的操作， 删除的元组不需要第一个列表
+'''
